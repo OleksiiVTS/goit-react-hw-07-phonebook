f457ab82344dff835c5e7eb1ed544159456d5a8f
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-07-28 18:22:59 +0300

    +

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,12 +1,14 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 import persistReducer from 'redux-persist/es/persistReducer';
 import storage from 'redux-persist/lib/storage';
-// const LS_KAY = 'list_contacts';
 
 export const contactsSlice = createSlice({
   name: 'contacts',
-  // initialState: JSON.parse(localStorage.getItem(LS_KAY)) ?? [],
-  initialState: { dataContacts: [] },
+  initialState: {
+    dataContacts: [],
+    isLoading: false,
+    error: null,
+  },
   reducers: {
     addContacts: {
       reducer(state, action) {
@@ -27,11 +29,25 @@ export const contactsSlice = createSlice({
       );
       state.dataContacts.splice(index, 1);
     },
+    // // Виконається в момент старту HTTP-запиту
+    // fetchingInProgress(state) {
+    //   state.isLoading = true;
+    // },
+    // // Виконається якщо HTTP-запит завершився успішно
+    // fetchingSuccess(state, action) {
+    //   state.isLoading = false;
+    //   state.error = null;
+    //   state.items = action.payload;
+    // },
+    // // Виконається якщо HTTP-запит завершився з помилкою
+    // fetchingError(state, action) {
+    //   state.isLoading = false;
+    //   state.error = action.payload;
+    // },
   },
 });
 
 export const { addContacts, deleteContacts } = contactsSlice.actions;
-// export const contactsReducer = contactsSlice.reducer;
 export const contactsReducer = persistReducer(
   {
     key: 'ist_contacts',
